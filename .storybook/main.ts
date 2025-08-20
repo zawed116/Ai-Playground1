6df1c08c7dce662f7de1149763e7b9a370babc06
Zawed <zawedakhter5786@gmail.com>
2025-08-20 22:29:46 +0530

    add

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,22 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest", // ✅ vitest addon here
   ],
-  "framework": {
-    "name": "@storybook/nextjs-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  staticDirs: ["../public"], // ✅ public assets
 };
+
 export default config;
